c7ca7bfa55e01b413cd8ab027a91955732b1bf7d
Berlin <tommyonik@gmail.com>
2024-02-28 22:00:29 -0500

    create Stuff model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+SIZES = (
+  ('S', 'Small'),
+  ('M', 'Medium'),
+  ('L', 'Large'),
+)
+
+class Stuff(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  size = models.CharField(
+    max_length=1,
+    choices=SIZES,
+    default=SIZES[0][0]
+  )
+  age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
