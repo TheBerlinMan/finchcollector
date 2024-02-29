d4716b36023822f659cae8823e7cb3a7998d4502
Berlin <tommyonik@gmail.com>
2024-02-28 22:21:04 -0500

    create stuff details page and complete part 3

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('stuff/', views.stuff_index, name='stuff-index'),
+    path('stuff/<int:stuff_id>/', views.stuff_detail, name='stuff-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from .models import Stuff
 
-# class Stuff:
-#   def __init__(self, name, type, description, size):
-#     self.name = name
-#     self.type = type
-#     self.description = description
-#     self.size = size
-
-# stuff = [
-#   Stuff('Dingus', 'Water', 'A water dingus.', 'Large'),
-#   Stuff('Crank', 'Fire', 'Will make your pee smelly.', 'Small'),
-#   Stuff('Snoch', 'Air', 'Call for a good time.', 'Very Small'),
-#   Stuff('Horg', 'Earth', 'The Stuff of Life.', 'Super Big')
-# ]
-
 def home(request):
   return render(request, 'home.html')
 
@@ -24,3 +10,7 @@ def about(request):
 def stuff_index(request):
   stuff = Stuff.objects.all()
   return render(request, 'stuff/index.html', {'stuff': stuff })
+
+def stuff_detail(request, stuff_id):
+  stuff = Stuff.objects.get(id=stuff_id)
+  return render(request, 'stuff/detail.html', { 'stuff': stuff })
