16e02364f66fc1e6945951acca88a8339e025aef
Berlin <tommyonik@gmail.com>
2024-03-05 20:37:53 -0500

    add auth to views

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Stuff, Things
 from .forms import WalkingForm
 
@@ -15,10 +17,12 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def stuff_index(request):
-  stuff = Stuff.objects.all()
+  stuff = Stuff.objects.filter(user=request.user)
   return render(request, 'stuff/index.html', {'stuff': stuff })
 
+@login_required
 def stuff_detail(request, stuff_id):
   stuff = Stuff.objects.get(id=stuff_id)
   things_stuff_doesnt_have = Things.objects.exclude(id__in = stuff.things.all().values_list('id'))
@@ -29,7 +33,7 @@ def stuff_detail(request, stuff_id):
     'things': things_stuff_doesnt_have
   })
 
-class StuffCreate(CreateView):
+class StuffCreate(LoginRequiredMixin, CreateView):
   model = Stuff
   fields = ['description', 'type', 'size', 'age']
   success_url='/stuff/'
@@ -39,14 +43,15 @@ class StuffCreate(CreateView):
     return super().form_valid(form)
   
 
-class StuffUpdate(UpdateView):
+class StuffUpdate(LoginRequiredMixin, UpdateView):
   model = Stuff
   fields = ['description', 'type', 'size', 'age']
 
-class StuffDelete(DeleteView):
+class StuffDelete(LoginRequiredMixin, DeleteView):
   model = Stuff
   success_url='/stuff/'
 
+@login_required
 def add_walk(request, stuff_id):
   form = WalkingForm(request.POST)
   if form.is_valid():
@@ -55,24 +60,25 @@ def add_walk(request, stuff_id):
     new_walk.save()
   return redirect('stuff-detail', stuff_id = stuff_id)
 
-class ThingsCreate(CreateView):
+class ThingsCreate(LoginRequiredMixin, CreateView):
   model = Things
   fields = '__all__'
 
-class ThingsList(ListView):
+class ThingsList(LoginRequiredMixin, ListView):
   model = Things
 
-class ThingsDetail(DetailView):
+class ThingsDetail(LoginRequiredMixin, DetailView):
   model = Things
 
-class ThingsUpdate(UpdateView):
+class ThingsUpdate(LoginRequiredMixin, UpdateView):
   model = Things
   fields = ['name', 'color']
 
-class ThingsDelete(DeleteView):
+class ThingsDelete(LoginRequiredMixin, DeleteView):
   model = Things
   success_url = '/things/'
 
+@login_required
 def assoc_things(request, stuff_id, things_id):
   Stuff.objects.get(id=stuff_id).things.add(things_id)
   return redirect('stuff-detail', stuff_id=stuff_id)
